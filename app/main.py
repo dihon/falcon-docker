c4dc16b7726a2d6b01076e114ed15dfd6d90e1e4
Dhon Rocafort <dhon@mokucollective.com>
2022-11-10 23:34:41 +0800

    Adding fix 1

--- app/main.py
+++ app/main.py
@@ -38,10 +38,10 @@ class DataResource:
         response.status = falcon.HTTP_200
         prm = request.params
 
-        ses_key = request.cookies.get('youfoo')
+        ses_key = request.context.get('youfoo')
         if ses_key == None or ses_key == '':
             ses_key = '%s - %s' % ( str(time.time()), hashlib.md5( str(time.time()).encode() ).hexdigest()[:-7])
-            response.set_cookie('youfoo', ses_key)
+            request.context.youfoo = ses_key
 
         config = {
           'user': 'root',
